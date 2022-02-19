585b7765a2e01026252553e8cc7638725edb8c22
Florentin Rieger <florentin.rieger@gmail.com>
2022-02-19 16:32:34 +0100

    opengl: feat(glew): added glew to get moden opengl support

--- hello-open-gl/src/Application.cpp
+++ hello-open-gl/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +21,11 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error initializing GLEW!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
